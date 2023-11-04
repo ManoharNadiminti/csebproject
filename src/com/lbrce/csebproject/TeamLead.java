a342f65dfed7317269a07d5f506d683218933878
91630 <91630@LAPTOP-F9QK994A>
2023-11-04 22:31:24 +0530

    first commit

--- src/com/lbrce/csebproject/TeamLead.java
+++ src/com/lbrce/csebproject/TeamLead.java
@@ -3,9 +3,9 @@ package com.lbrce.csebproject;
 public class TeamLead {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		manu();
+		srinu();
 	}
-	public static void manu() {
+	public static void srinu() {
 		// TODO Auto-generated method stub
 		System.out.println("This is Srinu");
 	}
