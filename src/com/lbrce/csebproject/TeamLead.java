d2961bfa518e4942d4e366fa96673bf404bd9462
91630 <91630@LAPTOP-F9QK994A>
2023-11-04 21:39:53 +0530

    first comment

---
+++ src/com/lbrce/csebproject/TeamLead.java
@@ -0,0 +1,14 @@
+package com.lbrce.csebproject;
+
+public class TeamLead {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		manu();
+	}
+	public static void manu() {
+		// TODO Auto-generated method stub
+		System.out.println("This is Srinu");
+	}
+
+
+}
